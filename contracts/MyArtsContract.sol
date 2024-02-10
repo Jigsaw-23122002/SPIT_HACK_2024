d1cc4c7dc3748382ef3322cef7ce57d08ba923a5
Harsh <harshnag23@gmail.com>
2024-02-10 12:57:41 +0530

    Arts section contract done and deployed.

---
+++ contracts/MyArtsContract.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract MyArtsToken {
+    uint256 public currentTokenId = 0;
+    uint256 public countOfTokensOnSale = 0;
+    mapping(uint256 => address) public tokenToUser;
+    mapping(address => uint256[]) public userToTokens;
+    mapping(uint256 => string) public metadataUri;
+    mapping(uint256 => uint256) public tokenCost;
+    mapping(uint256 => bool) public isTokenOnSale;
+
+    modifier isValidTokenId(uint256 _tokenId) {
+        require(
+            _tokenId >= 0 && _tokenId < currentTokenId,
+            "Invalid token Id passed into the function."
+        );
+        _;
+    }
+
+    function mint(string memory _metadata, uint256 _cost) public {
+        tokenToUser[currentTokenId] = msg.sender;
+        userToTokens[msg.sender].push(currentTokenId);
+        metadataUri[currentTokenId] = _metadata;
+        tokenCost[currentTokenId] = _cost;
+        currentTokenId++;
+    }
+
+    function buy(uint256 _tokenId) public payable isValidTokenId(_tokenId) {
+        require(isTokenOnSale[_tokenId] == true, "The token is not on sale.");
+        require(tokenCost[_tokenId] == msg.value, "Not enough ether sent.");
+        address to = tokenToUser[_tokenId];
+        tokenToUser[_tokenId] = msg.sender;
+        userToTokens[msg.sender].push(_tokenId);
+        isTokenOnSale[_tokenId] = false;
+        countOfTokensOnSale--;
+
+        for (uint256 i = 0; i < userToTokens[to].length; i++) {
+            if (userToTokens[to][i] == _tokenId) {
+                userToTokens[to][i] = userToTokens[to][
+                    userToTokens[to].length - 1
+                ];
+                userToTokens[to].pop();
+                break;
+            }
+        }
+        payable(to).transfer(msg.value);
+    }
+
+    function putTokenOnSale(uint256 _tokenId) public {
+        require(
+            tokenToUser[_tokenId] == msg.sender,
+            "You are not the owner of the token to put it on the sale."
+        );
+        require(isTokenOnSale[_tokenId] == false, "Token is already on sale.");
+        isTokenOnSale[_tokenId] = true;
+        countOfTokensOnSale++;
+    }
+
+    function getTokenDetails(
+        uint256 _tokenId
+    ) public view returns (string memory, uint256, bool, address) {
+        return (
+            metadataUri[_tokenId],
+            tokenCost[_tokenId],
+            isTokenOnSale[_tokenId],
+            tokenToUser[_tokenId]
+        );
+    }
+
+    function getMyTokens() public view returns (uint256[] memory) {
+        return userToTokens[msg.sender];
+    }
+
+    function getTokensOnSale() public view returns (uint256[] memory) {
+        uint256 current = 0;
+        uint256[] memory tokenIds = new uint256[](countOfTokensOnSale);
+
+        for (uint256 i = 0; i < currentTokenId; i++) {
+            if (isTokenOnSale[i] == true) {
+                tokenIds[current] = i;
+                current++;
+            }
+        }
+        return tokenIds;
+    }
+}
